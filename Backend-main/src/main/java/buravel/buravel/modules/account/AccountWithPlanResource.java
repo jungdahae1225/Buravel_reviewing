5a8a09fc148445579e3e9c34d4d67057ae133c00
jungdahae1225 <jungdahae1225@gmail.com>
2022-04-07 01:23:10 +0900

    reviewing

--- Backend-main/src/main/java/buravel/buravel/modules/account/AccountWithPlanResource.java
+++ Backend-main/src/main/java/buravel/buravel/modules/account/AccountWithPlanResource.java
@@ -14,7 +14,7 @@ public class AccountWithPlanResource extends EntityModel<AccountWithPlanDto> {
     public static EntityModel<AccountWithPlanDto> modelOf(AccountWithPlanDto target) {
         EntityModel<AccountWithPlanDto> accountResource = EntityModel.of(target);
         accountResource.add(linkTo(PlanController.class).withRel("create-plan"));
-         accountResource.add(linkTo(IndexController.class).slash("search").withRel("search"));
+        accountResource.add(linkTo(IndexController.class).slash("search").withRel("search"));
         accountResource.add(linkTo(PlanController.class).slash("mine").slash("closed").withRel("my-closed-plans"));
         accountResource.add(linkTo(PlanController.class).slash("mine").slash("published").withRel("my-published-plans"));
         accountResource.add(Link.of("https://docs.google.com/spreadsheets/d/1XmJZD9VyPquyhDm9XyfJL_KO89xLlN5-VTQGh6MTW-g/edit#gid=0").withRel("profile"));
